2da6f5f4801f49b177f93a8285a65d2ff3fc02de
Nilesh Deshmukh <dnilesh@intraedge.com>
2023-06-08 17:32:24 +0530

    deploy.sh workflow

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if [ "$MY_ENVIRONMENT" = "development" ]; then
+if [ "$MY_ENVIRONMENT" = "dev" ]; then
     echo "Deploying to development environment..."
     # Add commands for deploying to the development environment
-elif [ "$MY_ENVIRONMENT" = "production" ]; then
+elif [ "$MY_ENVIRONMENT" = "prod" ]; then
     echo "Deploying to production environment..."
     # Add commands for deploying to the production environment
 else
